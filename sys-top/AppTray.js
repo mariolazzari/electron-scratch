33d248488fef4565b471532ceeb8ab0d5ba8e8b7
Mario Lazzari <mario.lazzari@gmail.com>
2020-05-24 16:21:40 +0200

    Done lesson 40

--- sys-top/AppTray.js
+++ sys-top/AppTray.js
@@ -1,38 +1,38 @@
-const { app, Menu, Tray } = require('electron')
+const { app, Menu, Tray } = require("electron");
 
 class AppTray extends Tray {
   constructor(icon, mainWindow) {
-    super(icon)
+    super(icon);
 
-    this.setToolTip('SysTop')
+    this.setToolTip("SysTop");
 
-    this.mainWindow = mainWindow
+    this.mainWindow = mainWindow;
 
-    this.on('click', this.onClick.bind(this))
-    this.on('right-click', this.onRightClick.bind(this))
+    this.on("click", this.onClick.bind(this));
+    this.on("right-click", this.onRightClick.bind(this));
   }
 
   onClick() {
     if (this.mainWindow.isVisible() === true) {
-      this.mainWindow.hide()
+      this.mainWindow.hide();
     } else {
-      this.mainWindow.show()
+      this.mainWindow.show();
     }
   }
 
   onRightClick() {
     const contextMenu = Menu.buildFromTemplate([
       {
-        label: 'Quit',
+        label: "Quit",
         click: () => {
-          app.isQuitting = true
-          app.quit()
+          app.isQuitting = true;
+          app.quit();
         },
       },
-    ])
+    ]);
 
-    this.popUpContextMenu(contextMenu)
+    this.popUpContextMenu(contextMenu);
   }
 }
 
-module.exports = AppTray
+module.exports = AppTray;
--- sys-top/main.js
+++ sys-top/main.js
@@ -1,103 +1,103 @@
-const path = require('path')
-const { app, Menu, ipcMain } = require('electron')
-const Store = require('./Store')
-const MainWindow = require('./MainWindow')
-const AppTray = require('./AppTray')
+const path = require("path");
+const { app, Menu, ipcMain } = require("electron");
+const Store = require("./Store");
+const MainWindow = require("./MainWindow");
+const AppTray = require("./AppTray");
 
 // Set env
-process.env.NODE_ENV = 'production'
+process.env.NODE_ENV = "production";
 
-const isDev = process.env.NODE_ENV !== 'production' ? true : false
-const isMac = process.platform === 'darwin' ? true : false
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
 
-let mainWindow
-let tray
+let mainWindow;
+let tray;
 
 // Init store & defaults
 const store = new Store({
-  configName: 'user-settings',
+  configName: "user-settings",
   defaults: {
     settings: {
       cpuOverload: 80,
       alertFrequency: 5,
     },
   },
-})
+});
 
 function createMainWindow() {
-  mainWindow = new MainWindow('./app/index.html', isDev)
+  mainWindow = new MainWindow("./app/index.html", isDev);
 }
 
-app.on('ready', () => {
-  createMainWindow()
+app.on("ready", () => {
+  createMainWindow();
 
-  mainWindow.webContents.on('dom-ready', () => {
-    mainWindow.webContents.send('settings:get', store.get('settings'))
-  })
+  mainWindow.webContents.on("dom-ready", () => {
+    mainWindow.webContents.send("settings:get", store.get("settings"));
+  });
 
-  const mainMenu = Menu.buildFromTemplate(menu)
-  Menu.setApplicationMenu(mainMenu)
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
 
-  mainWindow.on('close', (e) => {
+  mainWindow.on("close", (e) => {
     if (!app.isQuitting) {
-      e.preventDefault()
-      mainWindow.hide()
+      e.preventDefault();
+      mainWindow.hide();
     }
 
-    return true
-  })
+    return true;
+  });
 
-  const icon = path.join(__dirname, 'assets', 'icons', 'tray_icon.png')
+  const icon = path.join(__dirname, "assets", "icons", "tray_icon.png");
 
   // Create tray
-  tray = new AppTray(icon, mainWindow)
-})
+  tray = new AppTray(icon, mainWindow);
+});
 
 const menu = [
-  ...(isMac ? [{ role: 'appMenu' }] : []),
+  ...(isMac ? [{ role: "appMenu" }] : []),
   {
-    role: 'fileMenu',
+    role: "fileMenu",
   },
   {
-    label: 'View',
+    label: "View",
     submenu: [
       {
-        label: 'Toggle Navigation',
-        click: () => mainWindow.webContents.send('nav:toggle'),
+        label: "Toggle Navigation",
+        click: () => mainWindow.webContents.send("nav:toggle"),
       },
     ],
   },
   ...(isDev
     ? [
         {
-          label: 'Developer',
+          label: "Developer",
           submenu: [
-            { role: 'reload' },
-            { role: 'forcereload' },
-            { type: 'separator' },
-            { role: 'toggledevtools' },
+            { role: "reload" },
+            { role: "forcereload" },
+            { type: "separator" },
+            { role: "toggledevtools" },
           ],
         },
       ]
     : []),
-]
+];
 
 // Set settings
-ipcMain.on('settings:set', (e, value) => {
-  store.set('settings', value)
-  mainWindow.webContents.send('settings:get', store.get('settings'))
-})
+ipcMain.on("settings:set", (e, value) => {
+  store.set("settings", value);
+  mainWindow.webContents.send("settings:get", store.get("settings"));
+});
 
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   if (!isMac) {
-    app.quit()
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createMainWindow()
+    createMainWindow();
   }
-})
+});
 
-app.allowRendererProcessReuse = true
+app.allowRendererProcessReuse = true;
